fd2b12c095bf127e6fbe6776e27217ed146ea56e
Thomas Ross <thomasross3610@gmail.com>
2019-09-17 15:24:41 +0100

    land method added

--- airport_js/spec/AirportSpec.js
+++ airport_js/spec/AirportSpec.js
@@ -5,9 +5,19 @@ describe('Airport', function() {
 
   beforeEach(function() {
     airport = new Airport();
+    plane = new Plane();
   });
 
   it('has no planes upon initialization', function() {
     expect(airport.planes.length).toEqual(0);
   });
+
+  it('has a default capacity', function() {
+    expect(airport.capacity).toEqual(20);
+  });
+
+  it('lands a plane', function() {
+    airport.landPlane(plane)
+    expect(airport.planes).toContain(plane);
+  })
 });
--- airport_js/src/Airport.js
+++ airport_js/src/Airport.js
@@ -2,3 +2,12 @@ function Airport(capacity = 20) {
   this.planes = []
   this.capacity = capacity
 };
+
+Airport.prototype.landPlane = function(plane) {
+  this.planes.push(plane);
+  return this.planes;
+};
+
+function Plane(name) {
+  this.name = name
+};
